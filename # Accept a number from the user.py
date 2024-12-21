0051b206fff964b63c830e25f90bbf3def7dac3a
lokesh782 <lokeshsingh7021@gmail.com>
2024-12-21 10:02:06 +0545

    Add files via upload

---
+++ # Accept a number from the user.py
@@ -0,0 +1,9 @@
+# Accept a number from the user
+number = int(input("Enter a number: "))
+
+# Check if the number is divisible by both 2 and 3
+if number % 2 == 0 and number % 3 == 0:
+    print("The number is divisible by both 2 and 3.")
+else:
+    print("The number is not divisible by both 2 and 3.")
+
