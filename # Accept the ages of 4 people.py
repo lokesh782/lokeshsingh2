701189155da7496c5f408c0e02487ea74f39a4a6
lokesh782 <lokeshsingh7021@gmail.com>
2024-12-21 20:30:31 +0545

    Update # Accept the ages of 4 people.py

--- # Accept the ages of 4 people.py
+++ # Accept the ages of 4 people.py
@@ -1,11 +1,10 @@
-# Accept the ages of 4 people
+
 age1 = int(input("Enter the age of person 1: "))
 age2 = int(input("Enter the age of person 2: "))
 age3 = int(input("Enter the age of person 3: "))
 age4 = int(input("Enter the age of person 4: "))
 
-# Find the youngest age using the min() function
 youngest_age = min(age1, age2, age3, age4)
 
-# Display the youngest age
+
 print("The youngest age is:", youngest_age)
